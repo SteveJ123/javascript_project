cefa44265df98bc345c066179dffa1987ea68e56
Stevej123 <jjanardhan@gmail.com>
2021-02-18 11:37:06 +0530

    getRandomNumber function added

--- color flipper/script.js
+++ color flipper/script.js
@@ -1,1 +1,9 @@
-console.log("test");
+const colors = ["green", "red", "rgb(133, 122, 200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+
+    
+})
